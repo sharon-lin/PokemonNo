89e7097e18aefcfea11854a412755cc3cf3ac0ba
Sharon <sharonlin14@gmail.com>
2016-07-19 00:27:54 -0400

    Add all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remove Pokemon from your Facebook Feed
+
+Remove all mentions of Pokemon Go from your Facebook Feed!
+
+Thanks to @derekstory.
---
+++ facebook_filter.js
@@ -0,0 +1,29 @@
+(function() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var newNodes = mutation.addedNodes;
+            if (newNodes !== null) {                
+                
+                var nodes = document.querySelectorAll('.userContentWrapper, ._1bar, ._5my2, ._4qjp, ._2kg4');
+                for (var ii = 0, nn = nodes.length; ii < nn; ii++)
+                {
+                    var text = nodes[ii] ? nodes[ii].textContent.toLowerCase() : '';
+                    if (text && text.indexOf('Pokemon Go') >= 0 && nodes[ii].style.display != 'none')
+                    {
+                        nodes[ii].style.display = 'none';
+                        chrome.runtime.sendMessage({action: "removePokemonGo"});
+                    }
+                }
+
+            }
+        });
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+    });
+
+})();
